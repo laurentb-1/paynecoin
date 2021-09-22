6e477cf2824e57dcab0a12541d8e208beaaed880
acarril <acarril@princeton.edu>
2021-09-22 14:24:47 -0400

    add first working version of simulation code

--- tests/simulation.py
+++ tests/simulation.py
@@ -4,18 +4,22 @@ import secrets
 from random import randrange
 from hashlib import sha256
 
-def req_endpoint(endpoint, data=None, port=5000):
+def req_endpoint(endpoint, port=5000, data=None):
+    # Check valid request
+    get_reqs = ['/nodes/resolve', '/chain', 'mine']
+    post_reqs = ['/nodes/register', '/transactions/new']
+    if endpoint not in (get_reqs and post_reqs):
+        print('invalid request')
+        return -1
+    # Determine request address and method
     base_url = f'http://localhost:{port}'
     url = f'{base_url}{endpoint}'
-    is_post = any(kwd in endpoint for kwd in ['transactions/new', 'nodes/register'])
+    is_post = any(kwd in endpoint for kwd in post_reqs)
     if is_post:
-        print('POST')
         req = requests.post(url, json=data)
     else:
-        print('GET')
         req = requests.get(url)
     return req.json()
-
 
 def simulate_transaction(sender=False, recipient=False, amount=False):
     sender = secrets.token_hex(16) if not sender else sender
@@ -28,11 +32,7 @@ def simulate_transaction(sender=False, recipient=False, amount=False):
     }
     return transaction
 
-
-nodes = {
-    'nodes': [f'http://localhost:{i}' for i in range(5000, 5020)]
-}
-
-req_endpoint('/transactions/new', data=simulate_transaction('quan', 'alvaro', 666))
+req_endpoint('/nodes/register')
+req_endpoint('/transactions/new', data=simulate_transaction('jonathan', 'alvaro', 42))
 req_endpoint('/mine')
 req_endpoint('/chain')
